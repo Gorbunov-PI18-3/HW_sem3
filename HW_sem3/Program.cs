0b87a2ec23bd6152e78599e5e38bfbfb77d2af8b
Gorbunov-PI18-3 <stfouley5@gmail.com>
2020-12-16 10:32:37 +0300

    Добавьте файлы проекта.

---
+++ HW_sem3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HW_sem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя: ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Введите возраст: ");
+            var age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Вас зовут {0} и ваш возраст {1}", name, age);
+
+            Console.WriteLine("Введите вашу дату рождения: ");
+            var birthdate = Console.ReadLine();
+            Console.WriteLine("Ваша дата рождения: {0}", birthdate);
+            Console.ReadKey();
+        }
+    }
+}
